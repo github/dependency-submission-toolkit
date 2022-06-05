6db395f2613ca4d5c4b89f4be7e5981f3ab0c506
Lane Seppala <lseppala@users.noreply.github.com>
2022-06-05 15:20:02 -0600

    Try action again

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Graph } from './graph'
 import { Manifest, BuildTarget } from './manifest'
 import { Metadata } from './metadata'
-import { Package } from './Package'
+import { Package } from './package'
 import { Snapshot, submitSnapshot } from './snapshot'
 
 export {
